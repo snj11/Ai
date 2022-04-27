ced0a05d2dc7c80ec2faad92dd0da6d84d3e9d3b
snj11 <89026734+snj11@users.noreply.github.com>
2022-04-27 06:15:26 +0530

    Add files via upload

---
+++ Ucs.py
@@ -0,0 +1,76 @@
+import sys
+
+
+class Graph:
+    def __init__(self):
+        self.adjDict = dict()
+        self.start_node = "S"
+        self.goal_node = "S"
+
+    def add_edge(self, u, v, w):
+        if u not in self.adjDict:
+            self.adjDict[u] = list()
+        self.adjDict[u].append([w, v])
+
+    def print(self):
+        for key in self.adjDict:
+            print(key)
+            print(self.adjDict[key])
+
+    def set_start_node(self, node):
+        self.start_node = node
+
+    def set_goal_node(self, node):
+        self.goal_node = node
+
+    def ucs(self):
+        max_distance = sys.maxsize
+
+        q = list()
+        visited = list()
+        q.append([0, self.start_node])
+        # print(q)
+        while q:
+            # print(q)
+            edge = q.pop(0)
+            # print(q)
+            if edge[1] in visited:
+                continue
+            visited.append(edge[1])
+            if edge[1] == self.goal_node:
+                if edge[0] < max_distance:
+                    max_distance = edge[0]
+            edges = []
+            if edge[1] in self.adjDict:
+                edges = self.adjDict[edge[1]]
+            for outEdge in edges:
+                # print(type(edge[0]))
+                # print(type(outEdge[0]))
+                q.append([edge[0] + outEdge[0], outEdge[1]])
+                q.sort()
+        print(f"min distance to goal node is: {max_distance}")
+
+
+def main():
+    print("Enter the graph with start node and child node separated by space and the cost written after a comma. Write '-1' to exit input of graph: ")
+    graph = Graph()
+    while True:
+        tokens = input().split()
+        parent = tokens[0]
+        if parent == "-1":
+            break
+        children = tokens[1:]
+        for child in children:
+            # print(child)
+            # print(child.split(","))
+            graph.add_edge(parent, child.split(",")[0], int(child.split(",")[1]))
+    # graph.print()
+    start_node = input("Enter start node: ")
+    goal_node = input("Enter goal node: ")
+    graph.set_start_node(start_node)
+    graph.set_goal_node(goal_node)
+    graph.ucs()
+    # graph.print()
+
+
+main()
